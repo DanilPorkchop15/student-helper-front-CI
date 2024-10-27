460a8d5516433b7080b4445085ff1fa9f970d73a
danilPorkchop15 <danilkostin150705@gmail.com>
2024-10-27 03:31:07 +0300

    feat(features): add theme ui

---
+++ src/features/theme/list/ui/ThemeListFeature.tsx
@@ -0,0 +1,17 @@
+import { memo } from "react";
+import { Flex } from "antd";
+import { useBranchDetails } from "entities/branch/model/branchDetails";
+import { ThemeView } from "entities/theme";
+import { useUniversityDetails } from "entities/university";
+
+export const ThemeListFeature = memo(function ThemeListFeature() {
+  const { id: universityId } = useUniversityDetails();
+  const { id: branchId, themes } = useBranchDetails();
+  return (
+    <Flex wrap gap={20} justify="space-between">
+      {themes.map((theme) => (
+        <ThemeView.Card key={theme.id} theme={theme} universityId={universityId} branchId={branchId} />
+      ))}
+    </Flex>
+  );
+});
